d7fc24e14d8f2a5cd7fb0a4b81c005e44f251387
Sparty93 <robertmarchiori@me.com>
2014-06-21 22:50:43 +1000

    First

--- CScd/GameScene.swift
+++ CScd/GameScene.swift
@@ -1,45 +1,75 @@
 //
 //  GameScene.swift
-//  CScd
+//  Tap That
 //
 //  Created by Milo on 6/21/14.
 //  Copyright (c) 2014 Spritley Games. All rights reserved.
 //
 
 import SpriteKit
+import QuartzCore
+
+var score = 0
+var nowTime : CFTimeInterval = CACurrentMediaTime()
+var gameOver : Bool = false
+let points = SKLabelNode(fontNamed:"Helvetica Neue UltraLight")
+let time = SKLabelNode(fontNamed:"Helvetica Neue UltraLight")
+let highPoints = SKLabelNode(fontNamed:"Helvetica Neue UltraLight")
 
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
         
-        self.addChild(myLabel)
+        
+        
+        points.fontSize = 175;
+        points.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame) / 2 * 1.5);
+        
+        self.addChild(points)
+        
+        time.fontSize = 175;
+        time.position = CGPoint(x:320, y:CGRectGetMidY(self.frame) / 4 * 1.5);
+        
+        self.addChild(time)
+        
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         /* Called when a touch begins */
         
+        if gameOver == true  {
+            
+            for touch: AnyObject in touches {
+                
+                score = 0
+                nowTime = CACurrentMediaTime()
+                gameOver = false
+                
+            }
+            
+        }
+        
         for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+            score++
         }
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        
+        var countDownTime  = String(format:"%.0f", (CACurrentMediaTime() - nowTime))
+
+        if (CACurrentMediaTime() - nowTime) < 5 {
+            time.text = "\(countDownTime)"
+            points.text = "\(score)"
+            self.backgroundColor = SKColor .greenColor()
+            
+        }
+        else {
+
+            gameOver = true
+            self.backgroundColor = SKColor .redColor()
+            
+        }
     }
 }
